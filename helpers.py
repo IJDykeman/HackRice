65a5242e3fee610ee31be3a5d9d0d05db74bebc8
IJDykeman <ijdykeman@gmail.com>
2015-01-31 13:38:36 -0600

    First commit

---
+++ helpers.py
@@ -0,0 +1,50 @@
+def get_num_rows(dbFile):
+	connection = sqlite3.connect(dbFile)
+	cursor = connection.cursor()
+	
+	#Print("TableName\tColumns\tRows\tCells")
+ 
+	totalTables = 0
+	totalColumns = 0
+	totalRows = 0
+	totalCells = 0
+	
+	# Get List of Tables:	  
+	tableListQuery = "SELECT name FROM sqlite_master WHERE type='table' ORDER BY Name"
+	cursor.execute(tableListQuery)
+	tables = map(lambda t: t[0], cursor.fetchall())
+	
+	for table in tables:
+	
+		
+			
+		columnsQuery = "PRAGMA table_info(%s)" % table
+		cursor.execute(columnsQuery)
+		numberOfColumns = len(cursor.fetchall())
+		
+		rowsQuery = "SELECT Count() FROM %s" % table
+		cursor.execute(rowsQuery)
+		numberOfRows = cursor.fetchone()[0]
+		
+		numberOfCells = numberOfColumns*numberOfRows
+		
+		#Print("%s\t%d\t%d\t%d" % (table, numberOfColumns, numberOfRows, numberOfCells))
+		
+		totalTables += 1
+		totalColumns += numberOfColumns
+		totalRows += numberOfRows
+		totalCells += numberOfCells
+ 
+	#Print( "" )
+	#Print( "Number of Tables:\t%d" % totalTables )
+	#Print( "Total Number of Columns:\t%d" % totalColumns )
+	#Print( "Total Number of Rows:\t%d" % totalRows )
+	#Print( "Total Number of Cells:\t%d" % totalCells )
+		
+	cursor.close()
+	connection.close()  
+	return totalRows 
+ 
+
+def get_hash(password):
+	return hashlib.sha1(password).hexdigest()
---
+++ script.py
@@ -0,0 +1,10 @@
+import random
+import string
+import sqlite3
+import cherrypy
+import hashlib
+from helpers import *
+
+USER_DB_STRING = "users.db"
+CARDS_DB_STRING = "cards.db"
+from string import Template
