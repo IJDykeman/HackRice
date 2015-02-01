4381beed98ea3619baf5be90a48b1e1acc2c1b8c
Patrick Girardet <pdg2@rice.edu>
2015-02-01 04:48:49 -0600

    Fixed Proposal Button Clicking
    Now 'Let's do it' -> 'I'm down' only if the mouse is actually over the
    button, also changed title of proposal page.

--- public/js/proposal_script.js
+++ public/js/proposal_script.js
@@ -3,7 +3,7 @@
  */
 $(document).ready(function(){
     $( "#events" ).accordion();
-    $('.success-button').click(function(){
+    $(".btn.btn-success.btn-sm").click(function(){
 
         $(this).replaceWith("<div align='right'><a href='#' class='btn btn-down btn-sm'>I'm down</a></div>");
     });
