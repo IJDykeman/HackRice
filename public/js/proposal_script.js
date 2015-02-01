c74d4018bb6dc836f8a311556e1962d274011ad7
Patrick Girardet <pdg2@rice.edu>
2015-02-01 02:03:10 -0600

    Proposal page accordion works
    Yup

--- public/js/proposal_script.js
+++ public/js/proposal_script.js
@@ -2,5 +2,10 @@
  * Created by pgirardet on 1/31/2015.
  */
 $(document).ready(function(){
-    $('#events').accordion();
+    $( "#events" ).accordion();
+    $('#events.success-button').click(function(){
+
+        (this).replaceWith("<a href='#' class='btn btn-danger'>Danger</a>");
+
+    });
 });
